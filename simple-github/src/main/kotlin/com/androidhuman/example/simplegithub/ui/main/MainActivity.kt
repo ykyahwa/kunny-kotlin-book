aad913e3c0a31d5ed51f5fea3e1be56f37ea6dc9
EokHyun Lee <ykyahwa@gmail.com>
2018-04-16 23:44:08 +0900

    1. Anko 적용

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/main/MainActivity.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/main/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.androidhuman.example.simplegithub.ui.main
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.androidhuman.example.simplegithub.R
 import com.androidhuman.example.simplegithub.ui.search.SearchActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnActivityMainSearch.setOnClickListener { startActivity(Intent(this@MainActivity, SearchActivity::class.java)) }
+        btnActivityMainSearch.setOnClickListener {
+            startActivity<SearchActivity>()
+        }
     }
 }
--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchActivity.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchActivity.kt
@@ -1,6 +1,5 @@
 package com.androidhuman.example.simplegithub.ui.search
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -16,6 +15,7 @@ import com.androidhuman.example.simplegithub.api.model.RepoSearchResponse
 import com.androidhuman.example.simplegithub.api.provideGithubApi
 import com.androidhuman.example.simplegithub.ui.repo.RepositoryActivity
 import kotlinx.android.synthetic.main.activity_search.*
+import org.jetbrains.anko.startActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -98,11 +98,10 @@ class SearchActivity : AppCompatActivity(), SearchAdapter.ItemClickListener {
     }
 
     override fun onItemClick(repository: GithubRepo) {
-        val intent = Intent(this, RepositoryActivity::class.java).apply {
-            putExtra(RepositoryActivity.KEY_USER_LOGIN, repository.owner.login)
-            putExtra(RepositoryActivity.KEY_REPO_NAME, repository.name)
-        }
-        startActivity(intent)
+        startActivity<RepositoryActivity>(
+                RepositoryActivity.KEY_USER_LOGIN to repository.owner.login,
+                RepositoryActivity.KEY_REPO_NAME to repository.name
+        )
     }
 
     private fun searchRepository(query: String) {
