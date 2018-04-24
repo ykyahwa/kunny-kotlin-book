9ce250795ad0c97a877e75dbf8cd949f5db3aa31
ehlee <ehlee@ysmcorp.com>
2018-04-24 13:12:45 +0900

    빌드 에러 수정

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/data/SearchHistoryDao.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/data/SearchHistoryDao.kt
@@ -1,5 +1,6 @@
 package com.androidhuman.example.simplegithub.data
 
+import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
@@ -9,12 +10,13 @@ import io.reactivex.Flowable
 /**
  * Created by eokhyunlee on 2018. 4. 20..
  */
+@Dao
 interface SearchHistoryDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun add(repo:GithubRepo)
 
-    @Query("SELECT * FROMREPOSITORIES")
+    @Query("SELECT * FROM REPOSITORIES")
     fun getHistory(): Flowable<List<GithubRepo>>
 
     @Query("DELETE FROM repositories")
